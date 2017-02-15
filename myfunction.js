53a0f5c55f2c95c52f0fb6bff3a583e6650148b2
lijunbo520 <838946433@qq.com>
2017-02-15 18:04:16 +0800

    2.14

--- myfunction.js
+++ myfunction.js
@@ -1,5 +1,6 @@
- 封装自己的函数
+// 封装自己的函数
  
+
  function getClass(classname)   //  类的写法
    {
        //判断支持否
@@ -24,6 +25,7 @@
        return arr;
    }
 
+
    // $("#demo")   $(".one")   $("div")
     //封装自己的$
     function $(str) {
@@ -41,3 +43,49 @@
                 return document.getElementsByTagName(str);
         }
     }
+
+
+    //封装自己的scrollTop left
+    function scroll() {
+        if(window.pageYOffset != null)  //  ie9+ 和其他浏览器
+        {
+            return {
+                left: window.pageXOffset,
+                top: window.pageYOffset
+            }
+        }
+        else if(document.compatMode == "CSS1Compat")  // 声明的了 DTD
+          // 检测是不是怪异模式的浏览器 -- 就是没有 声明<!DOCTYPE html>
+        {
+            return {
+                left: document.documentElement.scrollLeft,
+                top: document.documentElement.scrollTop
+            }
+        }
+        return { //  剩下的肯定是怪异模式的
+            left: document.body.scrollLeft,
+            top: document.body.scrollTop
+        }
+    }
+
+    //封装自己的clientWidth Height
+    function client() {
+				if(window.innerWidth != null)  // ie9 +  最新浏览器
+				{
+					return {
+						width: window.innerWidth,
+						height: window.innerHeight
+					}
+				}
+				else if(document.compatMode === "CSS1Compat")  // 标准浏览器
+				{
+					return {
+						width: document.documentElement.clientWidth,
+						height: document.documentElement.clientHeight
+					}
+				}
+				return {   // 怪异浏览器
+					width: document.body.clientWidth,
+					height: document.body.clientHeight
+				}
+			}
